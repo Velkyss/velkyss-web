2cfe8d20647ae86a36c9fa310a84c16b550efcb1
Ezequiel Marchena <ezmg92@gmail.com>
2026-03-29 21:21:54 +0200

    feat: Mejora menú en vistas móviles

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const form = document.getElementById('contact-form');
 const success = document.getElementById('form-success');
+const hamburger = document.getElementById('nav-hamburger');
+const navMobile = document.getElementById('nav-mobile');
 
 form.addEventListener('submit', async function(e) {
   e.preventDefault();
@@ -13,4 +15,14 @@ form.addEventListener('submit', async function(e) {
     form.style.display = 'none';
     success.style.display = 'flex';
   }
+});
+
+hamburger.addEventListener('click', function() {
+    navMobile.classList.toggle('open');
+});
+
+navMobile.querySelectorAll('a').forEach(function(link) {
+    link.addEventListener('click', function() {
+        navMobile.classList.remove('open');
+    });
 });
